10c506227117f1fec55776fe073df54a1de5240b
Juan Cruz <jc23729@gmail.com>
2020-12-16 19:17:59 +0000

    notes

--- main.py
+++ main.py
@@ -44,11 +44,12 @@ travel_log = {
 
 #Nesting Dictionary in a Dictionary
 
-travel_log = {
-  "France": {"cities_visited": ["Paris", "Lille", "Dijon"], "total_visits": 12},
-  "Germany": {"cities_visited": ["Berlin", "Hamburg", "Stuttgart"], "total_visits": 5},
-}
+# travel_log = {
+#   "France": {"cities_visited": ["Paris", "Lille", "Dijon"], "total_visits": 12},
+#   "Germany": {"cities_visited": ["Berlin", "Hamburg", "Stuttgart"], "total_visits": 5},
+# }
 
+"Germany": {"cities_visited": ["Berlin", "Hamburg", "Stuttgart"], "total_visits: 5",
 #Nesting Dictionaries in Lists
 
 travel_log = [
